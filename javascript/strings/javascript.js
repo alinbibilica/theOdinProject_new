24d687eda2337e6c518907a9008cee25ee3d5229
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-04-26 16:03:21 +0300

    strings...

---
+++ javascript/strings/javascript.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("button");
+
+function greet(){
+
+    const name = prompt("what is your name?");
+    const greeting = document.querySelector("#greeting");
+    greeting.textContent = `Hello ${name}, nice to see you!!`;
+
+    
+}
+
+button.addEventListener("click", greet);
