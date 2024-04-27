e316d30ce2952f43b6e3264174d1229df1408fa6
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-04-27 12:27:11 +0300

    javascript is fun!

--- javascript/strings/javascript.js
+++ javascript/strings/javascript.js
@@ -1,11 +1,29 @@
 const button = document.querySelector("button");
 
 function greet(){
-
     const name = prompt("what is your name?");
-    const greeting = document.querySelector("#greeting");
-    greeting.textContent = `Hello ${name}, nice to see you!!`;
+    
+    if (name==`Jackson`)
+    {
+        const greeting = document.querySelector("#greeting");
+        greeting.textContent = `Hello ${name}, nice to see you!!`;
+    }
 
+    else 
+    {
+        alert("Not owner of this computer!");
+        let question = prompt("Would you like to buy it?");
+
+        if  (question == "yes")
+        {
+            const greeting = document.querySelector("#greeting");
+            greeting.textContent = `that would be 50 buks ${name}`;
+        }
+        
+
+    }
+        
+        
     
 }
 
