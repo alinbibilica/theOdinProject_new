78fcd1b6e351ac528bf2030b4195eac607508b2b
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-05-17 13:11:49 +0300

    finished mp3 player

--- javascript/mp3Player/javascript.js
+++ javascript/mp3Player/javascript.js
@@ -5,6 +5,7 @@ const   play        = document.querySelector(".play"),
         trackImage  = document.querySelector(".track-image"),
         title       = document.querySelector(".title"),
         artist      = document.querySelector(".artist"),
+         
 
         trackCurrentTime    = document.querySelector(".current-time"),
         trackDuration       = document.querySelector(".duration-time"),
@@ -20,7 +21,8 @@ const   play        = document.querySelector(".play"),
         closeIcon    = document.querySelector(".fa-times"),
 
         musicPlaylist    = document.querySelector(".music-playlist"),
-        playlist         = document.querySelector(".playlist");
+        playlist         = document.querySelector(".playlist"),
+        pDiv             = document.querySelector(".playlist-div");
 
         let timer;
         let autoplay = 0;
@@ -39,6 +41,8 @@ const   play        = document.querySelector(".play"),
         currentVol.addEventListener("change", changeVolume);
         slider.addEventListener("change", changeDuration);
         track.addEventListener("timeupdate", songTimeUpdate);
+        hamburger.addEventListener("click", showPlaylist);
+        closeIcon.addEventListener("click", hidePlaylist);
 
         // loadtracks
         function loadTrack(indexTrack) {
@@ -50,6 +54,7 @@ const   play        = document.querySelector(".play"),
             trackImage.src = trackList[indexTrack].img;
             title.innerHTML = trackList[indexTrack].name;
             artist.innerHTML = trackList[indexTrack].singer;
+    
              
             track.load();
 
@@ -173,7 +178,9 @@ function updateSlider(){
 //updated current song time 
 
 function songTimeUpdate(){
-    let curmins = Math.floor(track.currentTime / 60);
+    if(track.duration)
+        {
+            let curmins = Math.floor(track.currentTime / 60);
     let cursecs = Math.floor(track.currentTime - curmins * 60);
 
     let durmins = Math.floor(track.duration / 60);
@@ -182,10 +189,71 @@ function songTimeUpdate(){
     if (dursecs < 10){
         dursecs = "0" + dursecs;
     }
-
+    
+    if (durmins < 10){
+        durmins = "0" + durmins;
+    }
     if (curmins < 10){
         curmins = "0" + curmins;
     }
 
- 
+    if (cursecs < 10){
+        cursecs = "0" + cursecs;
+    }
+    trackCurrentTime.innerHTML = curmins + ":" + cursecs;
+    trackDuration.innerHTML = durmins + ":" + dursecs;
+    } else {
+                trackCurrentTime.innerHTML = "00" + ":" + "00";
+                trackDuration.innerHTML = "00" + ":" + "00";
+            }
+
+}
+
+function showPlaylist () {
+        musicPlaylist.style.transform = "translateX(0)";
+}
+
+function hidePlaylist () {
+    musicPlaylist.style.transform = "translateX(-100%)";
+}
+
+//display tracks in playlist
+
+let counter = 1;
+
+function displayTracks(){
+    for (let i = 0; i < trackList.length; i++){
+        console.log(trackList[i].name + " - " + trackList[i].singer);
+    
+    let div = document.createElement("div");
+    div.classList.add("playlist");
+    div.innerHTML = `
+                     <span class="song-index">${counter++}</span>
+                     <p class="single-song">${trackList[i].name}</p>
+                    `;
+
+        pDiv.appendChild(div);
+    }
+   playFromPlaylist();
+}
+
+displayTracks();
+
+// Play song from playlist
+
+function playFromPlaylist(){
+    pDiv.addEventListener("click", (e) => {
+        if (e.target.classList.contains("single-song")){
+           // alert(e.target.innerHTML);
+           const indexNum = trackList.findIndex((item, index) => {
+            if (item.name === e.target.innerHTML) {
+                return true;
+            }
+           });
+           loadTrack(indexNum);
+           playSong();
+           hidePlaylist();
+        }
+    });
+   
 }
