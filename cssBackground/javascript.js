5a09b6225b964ae514a02ad6a6343676bb6fe25a
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-06-12 11:25:58 +0300

    access website based on age

---
+++ cssBackground/javascript.js
@@ -0,0 +1,40 @@
+let userAge;
+let parentAprove;
+let flag = false;
+let audio = document.getElementById("Audio");
+let background = document.getElementById("box");
+
+checkAge();
+playMusic(flag);
+
+
+
+function checkAge(){
+
+    userAge = prompt("How old are you?");
+    if(parseInt(userAge) > 18)
+        {
+            flag = true;
+        }
+    else
+        {
+            parentAprove = prompt("Do you have parent aproval?");
+            if ( parentAprove == 'yes')
+                flag = true;
+            else
+                flag = false;
+        }
+}
+    
+
+function playMusic(flag){
+
+    if(flag === true){
+        alert('Welcome!');
+    }
+    else 
+    {audio.style.display = 'none';
+     background.style.display =  'none';
+     alert("Not allowed on this website");
+    }
+}
