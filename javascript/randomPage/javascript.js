c17f8d3e53d2b655227422ce05c27ffcd9e81f5f
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-07-16 11:50:37 +0300

    random number added

--- javascript/randomPage/javascript.js
+++ javascript/randomPage/javascript.js
@@ -10,7 +10,7 @@ const lettersType = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o'
 const numbersType = ['0','1','2','3','4','5','6','7','8','9'];
 const symbolsType = ['!','@','#','$','%','^','&','*','(',')','?'];
 
- 
+let random = Math.random();
 
 let length = "";
 let numberOfLines = "";
