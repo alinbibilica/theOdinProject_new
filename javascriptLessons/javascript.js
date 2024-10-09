cdeca7f5266dc53e16cc3e10fae23386ed541c3e
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-10-09 23:27:33 +0300

    .... just saving my work

---
+++ javascriptLessons/javascript.js
@@ -0,0 +1,5 @@
+const fruits = ["bananas", "3", "portocale", "mango", "struguri"];
+const legumes = ["carrots","potatoes","tomatos"];
+fruits.push(legumes);
+
+document.getElementById("titlu").innerHTML=fruits.toString();
