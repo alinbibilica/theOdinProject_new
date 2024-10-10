785a2ae570b8544327eaad59587c9c3aed7a2fbb
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-10-10 11:42:25 +0300

    working with arrays ...

--- javascriptLessons/javascript.js
+++ javascriptLessons/javascript.js
@@ -1,5 +1,32 @@
 const fruits = ["bananas", "3", "portocale", "mango", "struguri"];
+fruits[1]  += " struguri";
 const legumes = ["carrots","potatoes","tomatos"];
-fruits.push(legumes);
+for(let y=0; y<legumes.length; y++){
+    fruits.push(legumes[y]);
+}
 
-document.getElementById("titlu").innerHTML=fruits.toString();
+for(const legume in legumes){console.log(legumes[legume])}
+
+
+const sliced = fruits.slice(2);
+console.log(sliced);
+
+const owner = {
+
+        firstName:"John",
+        lastName: "Mathwes",
+        dob: "1930",
+        adress: "5th street, Blvd Road , 23522, Sibiu",
+        cars: ["Bmw", "Mercedes", "Audi"],
+        currentYear: new Date().getFullYear(),
+ 
+  
+}
+
+ 
+
+let firstFruit = fruits[0];
+
+for (let i in fruits){document.getElementById("titlu").innerHTML+= "<li>" + fruits[i].toUpperCase()  + "</li>";}
+
+for (let y in owner.cars) {document.getElementById("hash2").innerHTML += "<li>" + owner.cars[y] + " in " + owner.currentYear + "</li>";}
