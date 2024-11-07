35db9f454be4f872d68d528c90bf63cde7ed7e7e
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-11-07 09:31:29 +0200

    dom...123

--- javascriptLessons/obj/js.js
+++ javascriptLessons/obj/js.js
@@ -12,7 +12,7 @@ container.appendChild(newDiv);
 // newDiv.style.height = "50px";
 
 /*set styles in one line*/
-container.style.cssText = "display: flex; flex-direction: row; flex-flow: row wrap; align-items: center; gap: 20px"
+container.style.cssText = "display: flex; flex-direction: column;   align-items: center; gap: 10px, width: auto; height: 90vh;"
 const button1 = document.createElement('button');
 button1.textContent = 'Counter';
 container.appendChild(button1);
@@ -22,9 +22,40 @@ resetBUtton.textContent ="Reset + Create";
 container.appendChild(resetBUtton);
 
 
+const testButton = document.createElement('button');
+testButton.textContent = "testing ...";
+container.appendChild(testButton);
+
+function random(number){
+    return Math.floor(Math.random() * (number+1));}
+
+
+
  newDiv.style.cssText = "color: gray; background: yellow; width: 50px; height: 50px; text-align: center; font-size: 3rem;"
  
+const buttons = document.querySelectorAll("button");
 
+buttons.forEach((button) =>{
+    button.addEventListener("click", ()=>{
+        const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+        container.style.background = rndCol;
+    });
+});
+
+
+
+
+
+ testButton.addEventListener("click", function(e){
+    
+                            if(e.target.style.background != "blue") {  e.target.style.background = "blue"}
+                            else e.target.style.background ="yellow";
+                                                    });
+
+container.addEventListener("click", function(e) {
+    console.log(`"Axa x: " + ${e.x}`); 
+    console.log(`"Axa y: " + ${e.y}`);
+});
 
 
 button1.addEventListener("click", ()=>{
@@ -39,16 +70,10 @@ resetBUtton.addEventListener("click", ()=>{
         numeButton = document.createElement('button');
         numeButton.textContent = x;
         container.appendChild(numeButton);
-
-    }
-
-
-    clickCounter="0";
-    newDiv.textContent = clickCounter;
-
-    
-
-                    })
+                                          }
+          clickCounter="0";
+          newDiv.textContent = clickCounter;
+                                                 })
 
 
  /* change background color on click */
