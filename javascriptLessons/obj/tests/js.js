4dae56480bf446935091c45ed0c48c220c10274b
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-11-09 00:18:32 +0200

    used an event object to change colors of all the elements in the header

--- javascriptLessons/obj/tests/js.js
+++ javascriptLessons/obj/tests/js.js
@@ -1,23 +1,17 @@
 let container1 = document.getElementById("container");
 
+let BtnOnClick = document.getElementById("BtnOnClick");
+let BtnAuto = document.getElementById("BtnAuto");
+let topButton = document.getElementById("topButton");
+let header = document.getElementById("text") ;
 
- 
 container1.style.cssText = " display:flex; flex-direction: row; justify-content: space-around;   align-items: self-end; background-image: linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%) ; overflow: hidden;";
  
 
 let lines = document.querySelectorAll(".lines");
 
 
-/*--------------------------------------------------
-sets all of them to the same size 
-
-lines.forEach(item =>{
-    item.style.width =`${divWidth}px`;
-    item.style.height =`${divHeight}vh`;
-    item.style.border = "1px solid black";
-});
-
----------------------------------------------------*/
+ 
  
 function iterate(lines){
 
@@ -31,10 +25,19 @@ function iterate(lines){
          }
 }
 
-function change(){iterate(lines);}
-
-setInterval(change, 1000);
+function change(){
+    iterate(lines);}
  
+setInterval(change, 1000);
+
+
+// topButton.addEventListener("click", changeBG);
+header.addEventListener("click", changeBG);
+
+ function changeBG(e){
+        e.target.style.background = `rgb(${randomNumber(255)} ${randomNumber(255)} ${randomNumber(255)})`;
+ }
+
 function randomNumber(number){
     return Math.floor(Math.random() * number + 10);
 }
