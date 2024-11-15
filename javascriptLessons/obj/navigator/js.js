1958183b59f09f98abeda82ffc3148a6754e9f9b
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-11-15 21:23:38 +0200

    navigator properties

---
+++ javascriptLessons/obj/navigator/js.js
@@ -0,0 +1,19 @@
+
+let userAgent = document.createElement('li');
+const language = document.createElement('li');
+const online = document.createElement('li');
+const vibrate =document.createElement('li');
+const oList = document.querySelector('ol');
+const browser = navigator;
+
+
+userAgent.textContent = `User-Agent: ${browser.userAgent}`;
+language.textContent =  `NetworkInfo: ${navigator.language}`;
+online.textContent =    `OnlineStatus: ${navigator.onLine?'Connected':'Disconnected'}`;
+vibrate.textContent = `Vibrate: ${navigator.vibrate([
+    100, 30, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100, 30, 100,
+  ])}`;
+ oList.appendChild(userAgent);
+ oList.appendChild(language);
+ oList.appendChild(online);
+ oList.appendChild(vibrate);
