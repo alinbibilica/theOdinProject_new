5984f2b7da4a12b023f05b72dc217bc304ce70e2
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-11-15 21:30:47 +0200

    ...

--- javascriptLessons/obj/navigator/js.js
+++ javascriptLessons/obj/navigator/js.js
@@ -10,17 +10,15 @@ const vibrateButton = document.getElementById('vibrationButton');
 userAgent.textContent = `User-Agent: ${browser.userAgent}`;
 language.textContent =  `NetworkInfo: ${navigator.language}`;
 online.textContent =    `OnlineStatus: ${navigator.onLine?'Connected':'Disconnected'}`;
-vibrate.textContent = `Vibrate: ${navigator.vibrate([
-    100, 30, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100, 30, 100,
-  ])}`;
+ 
  oList.appendChild(userAgent);
  oList.appendChild(language);
  oList.appendChild(online);
  oList.appendChild(vibrate);
 
- vibrateButton.addEventListener('click',vibrate);
+ vibrateButton.addEventListener('click',vibrate1);
 
-  function vibrate(){
+  function vibrate1(){
      navigator.vibrate([
         100, 30, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100, 30, 100,
       ]);
