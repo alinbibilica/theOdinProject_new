06f53663ffeb013754ed8a5ed3edfd4bd716c569
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-11-26 12:42:45 +0200

    touch screen tests

---
+++ javascriptLessons/obj/touchPage/javascript.js
@@ -0,0 +1,18 @@
+
+
+function update(event){
+    for (let dot; dot = document.querySelector('dot');){
+        dot.remove();
+    }
+    for(let i = 0; i< event.touches.legth; i++){
+        let {pageX, pageY} = event.touches[i];
+        let dot = document.createElement('dot');
+        dot.style.left = (pageX - 50) + "px";
+        dot.style.top = (pageY - 50) + "px";
+        document.body.appendChild(dot);
+    }
+}
+
+window.addEventListener('touchstart', update);
+window.addEventListener('touchmove', update);
+window.addEventListener('touchend', update);
