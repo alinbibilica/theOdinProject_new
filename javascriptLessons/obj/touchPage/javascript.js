caada43c0711034d92c49f6598e91772bc50c9ea
Alin Bibilica <89569089+alinbibilica@users.noreply.github.com>
2024-11-26 13:16:14 +0200

    heart shape

--- javascriptLessons/obj/touchPage/javascript.js
+++ javascriptLessons/obj/touchPage/javascript.js
@@ -1,10 +1,13 @@
 
 
 function update(event){
+
+    let touches = event.touches;
+
     for (let dot; dot = document.querySelector('dot');){
         dot.remove();
     }
-    for(let i = 0; i< event.touches.legth; i++){
+    for(let i = 0; i< touches.length; i++){
         let {pageX, pageY} = event.touches[i];
         let dot = document.createElement('dot');
         dot.style.left = (pageX - 50) + "px";
@@ -16,3 +19,4 @@ function update(event){
 window.addEventListener('touchstart', update);
 window.addEventListener('touchmove', update);
 window.addEventListener('touchend', update);
+window.addEventListener('mousedown', update);
